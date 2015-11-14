6d8e0f3d7f6f9a6a8511c3547c8ab519667a0718
Hans Vader <hassan@mail.blub>
2015-11-14 01:58:17 +0100

    using xset to set screen timeout in blurlock
    Former-commit-id: 4de4d8a65eb933c60abe246264e0bec3263e8e56

--- scripts/copy.sh
+++
@@ -1,7 +0,0 @@
-for i in ./* # iterate over all files in current dir
-do
-    if [ -d "$i" ] # if it's a directory
-    then
-        cp water.txt "$i" # copy water.txt into it
-    fi
-done
--- scripts/icc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/xcalib -d :0 /usr/share/color/icc/best.icm
