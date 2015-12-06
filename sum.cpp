24f9e529269f48b23d5d76eb834ed360b49fb02e
Hans Vader <hassan@mail.blub>
2015-12-06 22:39:12 +0100

    changed sum.cpp
    Former-commit-id: 9fb82c06d6dd35504cb88a78b2688ac615e1ea6b

--- sum.cpp
+++ sum.cpp
@@ -2,40 +2,32 @@
 #include <iostream>
 using namespace std;
 
-long sum1;
-long sum2;
-long sum3;
-long obergrenze, untergrenze;
+long sum1 = 0;
+long sum2 = 0;
+long sum3 = 0;
+long obergrenze = 1000;
+long untergrenze = 0;
 
-untergrenze = 0;
-obergrenze = 1000;
-
-sum1 = 0;
-sum2 = 0;
-sum3 = 0;
-
-void singleThread(long obgrenze, long ungrenze, long* sum) {
-	*sum = 0;
+void singleThread(long obgrenze, long ungrenze, long& sum) {
+	sum = 0;
 	for (int i = obgrenze; i <= ungrenze; i++) {
-		*sum += i;
-		cout << *sum << endl;
+		sum += i;
+		// cout << *sum << endl;
 	}
-	thread t1 = thread(singleThread, obgrenze, ungrenze);
-	t1.join();
 }
 
 void twoThreads() {
-	thread t1 = thread(singleThread, untergrenze , obergrenze / 2);
-	thread t2 = thread(singleThread, (obergrenze / 2) + 1, obergrenze);
+	thread t1 = thread(singleThread, untergrenze , obergrenze/2, ref(sum1));
+	thread t2 = thread(singleThread, (obergrenze/2)+1, obergrenze, ref(sum2));
 
 	t1.join();
 	t2.join();
 }
 
 void threeThreads() {
-	thread t1 = thread(summieren, untergrenze, obergrenze / 3);
-	thread t2 = thread(summieren, (obergrenze / 3) + 1, obergrenze / 3 * 2);
-	thread t3 = thread(summieren, (obergrenze / 3 * 2 + 1), obergrenze);
+	thread t1 = thread(singleThread, untergrenze, obergrenze/3, ref(sum1));
+	thread t2 = thread(singleThread, (obergrenze/3)+1, (obergrenze/3)*2, ref(sum2));
+	thread t3 = thread(singleThread, ((obergrenze/3) * 2)+1, obergrenze, ref(sum3));
 
 	t1.join();
 	t2.join();
@@ -43,7 +35,18 @@ void threeThreads() {
 }
 
 int main() {
-	cout << endl << sum1 + sum2 + sum3 << endl;
+    twoThreads();
+    long total = sum1 + sum2;
+	cout << endl << total << endl;
+
+    //partialsummen resetten
+    sum1 = 0;
+    sum2 = 0;
+    threeThreads();
+    total = sum1 + sum2 + sum3;
+    cout << endl << total << endl;
+
 	cin.get();
-	return EXIT_SUCCESS;
+	return 0;
 }
+
