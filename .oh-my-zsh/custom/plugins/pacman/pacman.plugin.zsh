1e1aae8b16d755af8b79a9bf5aa2a16111ae9e17
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2016-11-28 12:59:35 +0100

    added Sy alias

--- .oh-my-zsh/custom/plugins/pacman/pacman.plugin.zsh
+++ .oh-my-zsh/custom/plugins/pacman/pacman.plugin.zsh
@@ -1,6 +1,6 @@
 usercmd=(Ss Si Sg Sw Qs Qo Ql Qk Qc Qd Qe Qem Qm Qi Qkk Qu Qdt)
 aurcmd=(Ssa Sa Sia Syua Sua Swa)
-sudocmd=(S Syu Rns Syy Syyu)
+sudocmd=(S Syu Rns Sy Syy Syyu)
 
 for c in $usercmd; do; alias $c="pacman -$c"; done
 for c in $aurcmd; do; alias $c="pacaur -$c"; done
