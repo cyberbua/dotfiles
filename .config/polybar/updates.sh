4e065406c1a2a093ede2ac8e233ba0a1e109b5cc
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2016-12-27 19:19:26 +0100

    argument 'now' only forces update if more than 5 min ago

--- .config/polybar/updates.sh
+++ .config/polybar/updates.sh
@@ -12,7 +12,10 @@ fi
 NOW=$(date +%s)
 
 # if last update more than 3 hours ago or "now" argument used
-if (( (NOW - LASTUPDATE) > 10800 )) || [[ "$1" == "now" ]] ; then
+if (( (NOW - LASTUPDATE) > 10800 )); then
+    date +%s > lastupdate
+    checkupdates | wc -l | tee -a lastupdate
+elif (( (NOW - LASTUPDATE) > 300 )) && [[ "$1" == "now" ]]; then
     date +%s > lastupdate
     checkupdates | wc -l | tee -a lastupdate
 else
