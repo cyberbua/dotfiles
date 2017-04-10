b15b59179733db182d32accd43662d96443d0d08
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-04-10 16:34:07 +0200

    move general settings to xinitrc

--- xinit/.config/xstart.sh
+++ xinit/.config/xstart.sh
@@ -1,5 +1,3 @@
-setxkbmap -option 'caps:ctrl_modifier'   # caps = ctrl
-xcape -e 'Caps_Lock=Escape'              # ctrl = esc when tapped on its own
 xsetroot -cursor_name left_ptr           # set the default mouse cursor
 xmodmap -e "keycode 135 = Multi_key"
 feh --bg-scale ~/.config/bspwm/wallpaper # set the wallpaper
@@ -12,6 +10,4 @@ xfce4-power-manager
 /usr/bin/start-pulseaudio-x11 &
 xautolock -locker blurlock -time 3 -corners 0-00 &
 xset s off -dpms                         # disbale screen blanking (already handled by blurlock)
-export QT_STYLE_OVERRIDE=GTK+            # use GTK theme for Qt-Apps
 xhost +local:                            # allow local containers to access X
-xset r rate 200 30                       # key auto-repeat   # key auto-repeat
