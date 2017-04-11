88e4ee636e4976b85203c47b30be435e690bc081
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-04-11 02:19:17 +0200

    remove xfsettingsd because theming is handled by lxappearance now

--- xinit/.config/xstart.sh
+++ xinit/.config/xstart.sh
@@ -1,10 +1,8 @@
 xsetroot -cursor_name left_ptr           # set the default mouse cursor
-xmodmap -e "keycode 135 = Multi_key"
 feh --bg-scale ~/.config/bspwm/wallpaper # set the wallpaper
 nm-applet &
 compton -b
 redshift -l 16:48 -t 6500:4500 &
-xfsettingsd &
 polybar top &
 xfce4-power-manager
 /usr/bin/start-pulseaudio-x11 &
