e3e47f50e3b8a49d70f2072fa080828ec589b7d9
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2017-04-19 22:38:13 +0200

    add named pipe script as a wrapper for custom scripts; this makes it possible to update the modules text from within a click-handler

---
+++ polybar/.config/polybar/pipe.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# exit script on first error
+# set -o errexit
+
+# change to this scripts directory
+cd "$(dirname "$0")" || exit 1
+
+# delete named pipe when script is terminated
+trap "rm -f $1.pipe" EXIT INT TERM
+
+# create pipe if it doesn't exist
+if [[ ! -p $1.pipe ]]; then
+    mkfifo "$1.pipe"
+fi
+
+# read named pipe
+while [[ -p $1.pipe ]]; do
+    if read line < "$1.pipe"; then
+        [[ -n "$line" ]] && echo "$line"
+    fi
+done &
+
+# run the script periodically
+while [[ -p $1.pipe ]]; do
+    ./$1
+    sleep "$2"
+done
