d1fdbc71aacd3bd16d734e87972d2446c08b5f2c
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2018-02-17 15:08:40 +0100

    warmer colors at night

--- xinit/.config/xstart.sh
+++ xinit/.config/xstart.sh
@@ -2,7 +2,7 @@ xsetroot -cursor_name left_ptr           # set the default mouse cursor
 feh --bg-scale ~/.config/wallpaper       # set the wallpaper
 nm-applet &
 compton -b
-redshift -l 16:48 -t 6500:4500 &
+redshift -l 16:48 -t 6500:4300 &
 polybar top &
 xfce4-power-manager
 /usr/bin/start-pulseaudio-x11 &
