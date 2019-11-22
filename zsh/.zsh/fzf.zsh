e68d7ae170bf175a8522d04fd0353f88a2a5c261
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2019-11-22 21:20:12 +0100

    added modded version of pure prompt; zsh configs are now complete

---
+++ zsh/.zsh/fzf.zsh
@@ -0,0 +1,27 @@
+__fzfcmd() {
+    fzf --reverse --height 40% $@
+}
+
+_fzf-file-widget() {
+  local base=${${LBUFFER##* }:h}
+  local sel=$(find ${base} 2> /dev/null | __fzfcmd --query=${${LBUFFER##* }:t})
+  LBUFFER="${LBUFFER% *} ${sel}"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+
+_fzf-history-widget() {
+    local selected num
+    setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
+    selected=$(fc -rl 1 | __fzfcmd --query=${LBUFFER} --no-multi --tiebreak=index)
+    local ret=$?
+    if [ -n "$selected" ]; then
+        num=$selected[1]
+        if [ -n "$num" ]; then
+            zle vi-fetch-history -n $num
+        fi
+    fi
+    zle reset-prompt
+    return $ret
+}
