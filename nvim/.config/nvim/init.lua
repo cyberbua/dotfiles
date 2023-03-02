c8c4d8025a281c39d7f415a36610da429d038d73
Hassan Mohamad <hassan.mohamad.hm@gmail.com>
2023-03-03 00:06:20 +0100

    new lua nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,115 @@
+-- line breaks
+vim.opt.linebreak = true                -- break long lines at breakat-chars
+vim.opt.breakindent = true              -- maintain indent when breaking lines
+vim.opt.showbreak = '↳ '                -- symbol to show for line breaks
+vim.opt.breakindentopt = 'sbr'          -- showbreak symbol, then indent
+
+-- indentation
+vim.opt.tabstop = 4                     -- width of a tab
+vim.opt.shiftwidth = 4                  -- number of spaces for >> or <<
+vim.opt.softtabstop = 4                 -- <Tab> inserts spaces
+vim.opt.expandtab = true                -- same as softtabstop, but different
+vim.opt.autoindent = true               -- copy indentation for new lines
+
+-- search & replace
+vim.opt.ignorecase = true               -- ignore case when searching
+vim.opt.smartcase = true                -- except searches containing upper case
+
+-- misc
+vim.opt.number = true                   -- show line numbers
+vim.opt.relativenumber = true                   -- show line numbers
+vim.opt.scrolloff = 8                   -- maintain lines below/above cursor 
+vim.opt.pumheight = 15                  -- max size of completion menu
+vim.opt.wildmode = 'longest:full,full'  -- complete to longest match first
+vim.opt.virtualedit = 'block'           -- allow visual block in empty space
+vim.opt.cursorline = true               -- highlight the line where the cursor is
+vim.opt.foldmethod = 'marker'           -- fold at {{{ }}}
+vim.opt.autoread = true                 -- re-read file when changed externally
+vim.opt.splitbelow = true               -- create window-splits below
+vim.opt.splitright = true               -- create window-splits on the right
+vim.opt.list = true                     -- show certain whitespace characters
+vim.opt.listchars:append{trail = '•', tab = '| '}
+
+vim.opt.mousemodel = 'extend'           -- right mouse button extends selection
+vim.opt.mouse = 'a'                     -- enable mouse in all modes
+
+-- spell
+vim.opt.spellfile = '~/.config/nvim/spell/custom.utf-8.add'
+vim.opt.spelllang = 'de,en'
+vim.opt.complete:append('kspell')
+
+
+-- mappings
+vim.g.mapleader = " "
+
+-- helper function for setting keymaps with default values
+function map(mode, lhs, rhs, opts)
+    local defaults = {
+        silent = true,
+    }
+    if opts then
+        defaults = vim.tbl_extend("force", defaults, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, defaults)
+end
+
+-- move around wrapped lines easily
+map('n', 'j', 'gj', opts)
+map('n', 'k', 'gk', opts)
+
+-- indent visual selections
+map('x', '>', '>gv', opts)
+map('x', '<', '<gv', opts)
+
+-- use . on multiple line
+map('x', '.', ':norm .<cr>', opts)
+
+-- visual selection -> primary clipboard
+map('x', '<LeftRelease>', '"*ygv', opts)
+
+-- use <leader> + yank/paste for secondary clipboard
+map('', '<leader>y', '"+y', {desc = 'yank to system clipboard'})
+map('', '<leader>p', '"+p', {desc = 'paste from system clipboard'})
+map('', '<leader>P', '"+P', {desc = 'Paste from system clipboard'})
+
+-- move between buffers
+map('n', '<c-l>', ':bnext<cr>', opts)
+map('n', '<c-h>', ':bprevious<cr>', opts)
+map('n', '<tab>', ':b#<cr>', opts)
+
+-- save with ctrl-s
+map('n', '<c-s>', ':update<cr>', opts)
+map('i', '<c-s>', '<esc>:update<cr>', opts)
+
+-- remove search highlighting with <esc>
+map('n', '<esc>', ':nohlsearch<return><esc>', opts)
+
+vim.cmd([[
+function! s:closeorquit()
+    if len(filter(range(1, bufnr('$')), 'buflisted(v:val)')) > 1
+        bdelete
+    else
+        quit
+    endif
+endfunc
+nnoremap <silent> <C-Q> :call <sid>closeorquit()<CR>
+]])
+
+-- autocmds
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = {'md','markdown','latex','tex','gitcommit','plaintex'},
+    callback = function()
+        vim.cmd.setlocal('spell')
+    end
+})
+
+-- plugin manager
+-- install with this command:
+-- git clone --filter=blob:none "https://github.com/folke/lazy.nvim.git" --branch=stable ~/.local/share/nvim/lazy/lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if vim.loop.fs_stat(lazypath) then
+    vim.opt.runtimepath:prepend(lazypath)
+    require('plugins')
+else
+    vim.cmd.colorscheme('habamax')
+end
---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,136 @@
+-- vim: set tabstop=2 shiftwidth=2 softtabstop=2:
+require("lazy").setup({
+  {
+    "navarasu/onedark.nvim",
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    init = function()
+      require('onedark').load()
+    end,
+    opts = {
+      style = 'dark'
+    }
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = true,
+    opts = {
+      signcolumn = true
+    }
+  },
+
+  {
+    "folke/which-key.nvim",
+    enabled = true,
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup({
+        plugins = {
+          spelling = true
+        },
+        operators = { gc = "Comments" },  -- does thi do anything?
+      })
+    end,
+  },
+
+  {
+    'echasnovski/mini.comment',
+    enabled = true,
+    config = function()
+      require("mini.comment").setup()
+    end
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    enabled = true,
+    config = function()
+      require("nvim-autopairs").setup({ })
+    end
+
+  },
+
+  {
+    "kylechui/nvim-surround",
+    enabled = true,
+    config = function()
+      require("nvim-surround").setup({ })
+    end
+
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    enabled = true,
+    config = function()
+      require("lualine").setup({
+        options = {
+          icons_enabled = false,
+          component_separators = { left = '|', right = '|'},
+          section_separators = { left = '', right = ''},
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'%B', 'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'%l/%L:%3v'}
+        },
+        tabline = {
+          lualine_b = {'buffers'},
+          lualine_y = {'tabs'}
+        },
+      })
+    end
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    enabled = true,
+    build = ':TSUpdate',
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {'bash', 'bibtex', 'c', 'cpp', 'css', 'diff', 'gitcommit', 'gitignore', 'go', 'help', 'html', 'ini', 'java', 'javascript', 'json', 'json5', 'jsonc', 'latex', 'make', 'markdown', 'markdown_inline', 'perl', 'php', 'python', 'regex', 'rust', 'smali', 'sql', 'toml', 'typescript', 'vim', 'yaml'},
+        highlight = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<cr>",
+            node_incremental = "<cr>",
+            scope_incremental = "<tab>",
+            node_decremental = "<s-tab>",
+          },
+        },
+      })
+    end
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-refactor",
+    enabled =true,
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
+    config = function()
+      vim.opt.updatetime = 1000   -- used for highlight_definitions
+      require("nvim-treesitter.configs").setup({
+        refactor = {
+          highlight_definitions = { enable = true},
+          smart_rename = {
+            enable = true,
+            keymaps = {
+              smart_rename = "<leader>r"
+            },
+          }
+        },
+      })
+    end
+  }
+
+})
+
